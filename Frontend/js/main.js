bec99a9b941c0c2675723dedfa0442a26fd44581
Agustín Ammazzagatti <aammazzagatti@IMA-CBA.COM.AR>
2023-10-14 10:58:44 -0300

    productos.html creado

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,10 +1,37 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+/* Sección index.html */
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    navbar.classList.toggle('active');
-}
+  navbar.classList.toggle("active");
+};
 
 window.onscroll = () => {
-    navbar.classList.remove('active');
-}
+  navbar.classList.remove("active");
+};
+
+/* Sección productos.html */
+
+let preveiwContainer = document.querySelector(".products-preview");
+let previewBox = preveiwContainer.querySelectorAll(".preview");
+
+document.querySelectorAll(".products-container .product").forEach((product) => {
+  product.onclick = () => {
+    preveiwContainer.style.display = "flex";
+    let name = product.getAttribute("data-name");
+    previewBox.forEach((preview) => {
+      let target = preview.getAttribute("data-target");
+      if (name == target) {
+        preview.classList.add("active");
+      }
+    });
+  };
+});
+
+previewBox.forEach((close) => {
+  close.querySelector(".fa-times").onclick = () => {
+    close.classList.remove("active");
+    preveiwContainer.style.display = "none";
+  };
+});
